f16bd7fd00a7e4b6eb87e5c7ae7a33ac53fa294a
topaznoam <noamtopaz@gmail.com>
2024-07-13 19:00:12 +0300

    last-update

--- src/components/ShowCard.tsx
+++ src/components/ShowCard.tsx
@@ -6,47 +6,29 @@ type ShowCardProps = {
 };
 
 const ShowCard: React.FC<ShowCardProps> = ({ name }) => {
-  const [flipped, setFlipped] = useState<boolean>(false);
+  const [flipped, setFlipped] = useState(false);
 
   useEffect(() => {
-    if (name !== BACK_CARD && name !== "") {
-      const timer = setTimeout(() => {
-        setFlipped(true);
-      }, 250);
+    if (name && name !== BACK_CARD) {
+      const timer = setTimeout(() => setFlipped(true), 250);
       return () => clearTimeout(timer);
     }
   }, [name]);
 
-  if (name !== "") {
-    if (flipped) {
-      return (
-        <div className="flip-card flipped">
-          <div className="flip-card-inner">
-            <div className="flip-card-front">
-              <img src={"/images/cards/back.png"} alt="back" />
-            </div>
-            <div className="flip-card-back">
-              <img src={`/images/cards/${name}.png`} alt={name} />
-            </div>
+  if (!name) return null;
+  else {
+    return (
+      <div className={`flip-card ${flipped ? "flipped" : ""}`}>
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src="/images/cards/back.png" alt="back" />
+          </div>
+          <div className="flip-card-back">
+            <img src={`/images/cards/${name}.png`} alt={name} />
           </div>
         </div>
-      );
-    } else {
-      return (
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div className="flip-card-front">
-              <img src={"/images/cards/back.png"} alt="back" />
-            </div>
-            <div className="flip-card-back">
-              <img src={`/images/cards/${name}.png`} alt={name} />
-            </div>
-          </div>
-        </div>
-      );
-    }
-  } else {
-    return null;
+      </div>
+    );
   }
 };
 
